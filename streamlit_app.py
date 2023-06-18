3a3b4feebb101ecd2b699d6f01fd2e1bfe35afec
Ikechukwu Chilaka <123249785+Aykechilaka@users.noreply.github.com>
2023-06-18 12:25:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalize the data to make it look better
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the normalized version
+streamlit.dataframe(fruityvice_normalized)
