4c76c706ad2dc064d59ee9ea25399781c270a1e5
Ikechukwu Chilaka <123249785+Aykechilaka@users.noreply.github.com>
2023-06-21 13:37:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,14 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?' 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # streamlit.text(fruityvice_response.json())
-
 # normalize the data to make it look better
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display the normalized version
@@ -41,7 +43,6 @@ try:
   if not fruit_choice:
     sreamlit.error("please select a fruit to get information.")
   else:
-    #streamlit.write('The user entered ', fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
