26dec481a0772b3071153f621a204aca3a324894
Ikechukwu Chilaka <123249785+Aykechilaka@users.noreply.github.com>
2023-06-22 09:36:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,23 +37,23 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-#create the repeatable code block (called function)
-def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   return fruityvice_normalized
-#New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    sreamlit.error("please select a fruit to get information.")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+# #create the repeatable code block (called function)
+# def get_fruityvice_data(this_fruit_choice):
+#    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#    return fruityvice_normalized
+# #New section to display fruityvice api response
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     sreamlit.error("please select a fruit to get information.")
+#   else:
+#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#     streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#   streamlit.error()
 
 
 #don't run anything past here while we troubleshoot
